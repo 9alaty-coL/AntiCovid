3257a311d24e8ab07cef156eb57fa16246ed62aa
Tr-nT-nL-c <66482009+GaLaTiCosS@users.noreply.github.com>
2021-11-30 20:32:20 +0700

    redirect

--- src/routes/authen.js
+++ src/routes/authen.js
@@ -5,4 +5,8 @@ const AuthenController = require('../app/controllers/AuthenController');
 
 router.get('/sign-in', AuthenController.signIn);
 
+router.get('/', (res, req, next) => {
+    return res.redirect('/sign-in');
+});
+
 module.exports = router;
