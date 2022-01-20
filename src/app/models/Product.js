c88170b54b56b5513c56de4432515df7f0987dd5
Le Minh Hau <minhhaumn46@gmail.com>
2022-01-20 14:08:37 +0700

    chinh sưa add product

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -74,6 +74,15 @@ class Products {
             return null;
         }
     }
+    async nextID() {
+        try{
+            const res = await db.maximum('Product_ID', tbName);
+            return res + 1;
+        }catch(err){
+            console.log('error in account next: ' + err.message);
+            return null;
+        }
+    }
 }
 
 module.exports = new Products();
