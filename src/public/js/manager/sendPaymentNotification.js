04f7e0121345bd11c073bf4ba8dc8f5e05dc5aff
DoriTran <tranquocdong308@gmail.com>
2022-01-20 10:01:23 +0700

    Almost done Notification Payment Reminder

---
+++ src/public/js/manager/sendPaymentNotification.js
@@ -0,0 +1,20 @@
+
+
+$('.change-min-payment').submit(function(e) {
+    e.preventDefault();
+    
+    // $(this).serialize(); will be the serialized form
+    let data = $(this).serialize();
+    let key = data.split('&');
+    let couple = {};
+    couple.P_ID = key[0].split('=')[1];
+    
+    $(`#${couple.P_ID}`).text(`${couple.P_MinPayment}`);
+
+    // Submit
+    $.ajax({
+        url: '/manager/putChangeMinPayment',
+        type: 'PUT',
+        data: data,
+    });
+});
